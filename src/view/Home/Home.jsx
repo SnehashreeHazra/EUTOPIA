d6c75e31ce32932ab260dc8622f3e6a9c0481dd9
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-24 16:42:45 +0530

    'up'

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import logo from "../../assets/Eutopia-logo 1.png"
+import logo from "../../assets/Eutopia-logo 1.png";
 
 const Home = () => {
   return (
@@ -8,13 +8,26 @@ const Home = () => {
       <div className="home-banner">
         <div className="uni-container">
           <div className="header">
-            <img src={logo} alt="" />
+            <img className="header-logo" src={logo} alt="" />
             <div className="navbar">
-              <p>Home</p>
+              <p className="active">Home</p>
               <p>About Us</p>
               <p>Pricing</p>
             </div>
             <button className="header-btn">Get Free Demo</button>
+          </div>
+          <div className="home-content">
+            <p>
+              Eutopia school manage ment system—the only software you'll need
+            </p>
+            <p>
+              Source, screen, and place top talent faster with automation that
+              works like your best recruiter.
+            </p>
+            <div>
+              <button>Get Early Access</button>
+              <button>Request a Demo</button>
+            </div>
           </div>
         </div>
       </div>
