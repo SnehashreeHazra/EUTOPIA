eb08b65dbc0777b88d3a3a7f09cb41cbf34cf85d
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-24 15:49:41 +0530

    'up'

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -4,7 +4,11 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home'>
-      
+      <div className="home-banner">
+        <div className="uni-container">
+          
+        </div>
+      </div>
     </div>
   )
 }
